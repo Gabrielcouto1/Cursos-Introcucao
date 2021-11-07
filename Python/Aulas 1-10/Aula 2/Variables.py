0d31feb947f3e963d0eef7fc089b8b889fcc9e16
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-07 19:14:49 -0300

    Main commit

---
+++ Python/Aulas 1-10/Aula 2/Variables.py
@@ -0,0 +1,25 @@
+first_name = "Gabriel"
+last_name = "Couto"
+full_name = first_name + " " + last_name
+
+print("\n\nHello "+full_name)
+#print(type(name))
+
+
+age = 18
+age += 1
+
+print("Your age is: " + str(age))
+#print(type(age))
+
+
+height = 171.5
+
+print("Your height is: " + str(height) + " cm.")
+#print(type(height))
+
+
+human = True
+
+print("Are you a human? "+ str(human))
+#print(type(human))
