b526627a6e7ad89797d02cde3b4c3727ed988e4b
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-07 21:37:43 -0300

    Main commit

--- Python/Aulas 1-10/Aula 9/if.py
+++ Python/Aulas 1-10/Aula 9/if.py
@@ -1,6 +1,8 @@
 age = int(input("How old are you?: "))
 
-if age>=18:
+if age==100:
+    print("You are a century old!")
+elif age>=18:
     print("You are an adult.")
 elif age < 0: 
     print("You haven't been born yet.")
