fa962a20a242b53de7d3ee69f1a5ac3ab7a5f4a3
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-08 23:33:37 -0300

    Main commit

---
+++ Python/Aulas 11-20/Aula 18/sets.py
@@ -0,0 +1,14 @@
+#set = collection which is unordered, unindexed. No duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+#utensils.add("napkin")
+#utensils.remove("fork")
+#utensils.clear()
+#dishes.update(utensils)
+#dinner_table = utensils.union(dishes)
+print(utensils.difference(dishes))     #will print what utelsils has that dishes doesnt
+
+#for i in dinner_table:
+#    print(i)
