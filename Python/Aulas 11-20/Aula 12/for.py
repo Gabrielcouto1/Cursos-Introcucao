1e5796e147adf40f71b23093ae73f819a29a1d36
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-08 22:35:00 -0300

    Main commit

---
+++ Python/Aulas 11-20/Aula 12/for.py
@@ -0,0 +1,24 @@
+# for loop =    a statement that will execute it's block of code
+#                      a limited amount of times
+#
+#                     while loop = unlimited
+#                     for loop = limited
+
+import time         # time.sleep()
+
+#for i in range(10):
+#    print(i+1)
+
+#for i in range(50,101):
+#    print(i)
+
+#for i in range (0,101,2):
+#    print(i)
+
+#for i in "Gabriel Couto":
+#    print(i)
+
+for i in range (10,0,-1):
+    print(i)
+    time.sleep(1)   # waits 1 second to continue the program
+print("\nHappy new year!!")
