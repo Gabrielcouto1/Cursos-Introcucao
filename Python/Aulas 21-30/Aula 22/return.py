3d9b83b4cbbbcf33acdffca0685f206935fc85e3
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-10 00:09:02 -0300

    Main commit

---
+++ Python/Aulas 21-30/Aula 22/return.py
@@ -0,0 +1,9 @@
+#return statement = Functions send Python values/objects back to the caller.
+#                    These values/objects are known as the function’s return value
+
+def multiply(x,y):
+    return x*y
+
+result = multiply(1,32) 
+
+print(result)
