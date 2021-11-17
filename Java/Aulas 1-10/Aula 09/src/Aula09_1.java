0a514027a2710c70a1b7801dfd445294e064e473
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-11-17 20:27:57 -0300

    Main commit

---
+++ Java/Aulas 1-10/Aula 09/src/Aula09_1.java
@@ -0,0 +1,21 @@
+//if statements
+
+public class Aula09_1 {
+    public static void main(String[] args) {
+        
+        int age = 56;
+
+        if (age >= 75)
+            System.out.println("Okay boomer!");
+        
+        else if (age >=18)
+            System.out.println("You are an adult.");
+        
+        else if (age <=13)
+            System.out.println("You are a teenager.");
+        
+        else 
+            System.out.println("You are not an adult.");
+        
+    }
+}
